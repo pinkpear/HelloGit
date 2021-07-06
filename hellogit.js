88e3817a883772ce0d567e398f55956796a44bc8
agonyperkey <314068298@qq.com>
2021-07-06 21:40:14 +0800

    hello

--- hellogit.js
+++ hellogit.js
@@ -1,10 +1,5 @@
-var a = "hello";
-console.log(a + " Git");
-function Dog(name){
-  this.name = name;
-}
-
-var dog = new Dog("小黑");
-console.log(dog.name);
-console.log(Dog.prototype);
-iji
+在这里面写的东西
+可以被
+全世界的人
+看到
+hhhhhh～
